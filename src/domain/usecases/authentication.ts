2f7a0c6ff10dea878cca0f771dc385de8f21aee0
Vagner <vagnerbuzatta@hotmail.com>
2020-09-09 22:37:47 -0300

    chore: add interface auth

--- src/domain/usecases/authentication.ts
+++ src/domain/usecases/authentication.ts
@@ -1,11 +1,13 @@
 import { AccountModel } from '../models/account.model'
-
-type AuthenticationParams = {
-    email: string
-    password: string
+export interface Authentication {
+    auth: (params: Authentication.Params) => Promise<Authentication.Model>
 }
 
+export namespace Authentication {
+    export type Params = {
+        email: string
+        password: string
+    }
 
-export interface Authentication {
-    auth: (params: AuthenticationParams) : Promise<AccountModel>
+    export type Model = AccountModel
 }
